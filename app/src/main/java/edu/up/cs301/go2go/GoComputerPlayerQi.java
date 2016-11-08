881279cac1e8bed758deaaa15b07058a0c07aa81
SamSymmes <symmes19@up.edu>
2016-11-08 13:41:29 -0800

    Create GoComputerPlayerQi.java

---
+++ app/src/main/java/edu/up/cs301/go2go/GoComputerPlayerQi.java
@@ -0,0 +1,17 @@
+package edu.up.cs301.go2go;
+
+import java.util.ArrayList;
+
+import edu.up.cs301.game.GameComputerPlayer;
+import edu.up.cs301.game.infoMsg.GameInfo;
+
+public class GoComputerPlayerQi extends GameComputerPlayer {
+    public GoComputerPlayerQi(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void receiveInfo(GameInfo info) {
+    
+    }
+}
