8942abd4589677797a9c2ca6f316b8129671a20e
SamSymmes <symmes19@up.edu>
2016-11-08 13:42:29 -0800

    Create GoComputerPlayer1.java

---
+++ app/src/main/java/edu/up/cs301/go2go/GoComputerPlayer1.java
@@ -0,0 +1,21 @@
+package edu.up.cs301.go2go;
+
+import java.util.ArrayList;
+
+import edu.up.cs301.game.GameComputerPlayer;
+import edu.up.cs301.game.infoMsg.GameInfo;
+
+/**
+ * Sam Symmes
+ * November 8th 2016
+ */
+
+public class GoComputerPlayer1 extends GameComputerPlayer {
+    public GoComputerPlayer1(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void receiveInfo(GameInfo info) {
+    
+    }
