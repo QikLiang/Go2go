ddabfa82ea6339a280df6f1cfd43eb2c525a5bf3
qikliang <qik.liang@gmail.com>
2016-11-12 16:08:03 -0800

    Create new logo

--- app/build/generated/source/r/debug/edu/up/cs301/game/R.java
+++ app/build/generated/source/r/debug/edu/up/cs301/game/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int table_row_vert_pad=0x7f060005;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int purple_delete_button=0x7f020002;
+        public static final int go_board=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int purple_delete_button=0x7f020003;
     }
     public static final class id {
         public static final int addPlayerButton=0x7f09000d;
